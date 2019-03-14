eac642615e9419fb87d37a1ae9e7ac2a0a01ef99
GuoPing666 <45675797+GuoPing666@users.noreply.github.com>
2019-03-14 09:44:12 +0800

    first commit

---
+++ 纯JS练习气泡箭头.html/indexA.js
@@ -0,0 +1,24 @@
+﻿function A() {
+    document.getElementById("a1").style.display = "block"
+    document.getElementById("a2").style.display = "none"
+    document.getElementById("a3").style.display = "none"
+    document.getElementById("a4").style.display = "none"
+}
+function B() {
+    document.getElementById("a2").style.display = "block"
+    document.getElementById("a1").style.display = "none"
+    document.getElementById("a3").style.display = "none"
+    document.getElementById("a4").style.display = "none"
+}
+function C() {
+    document.getElementById("a3").style.display = "block"
+    document.getElementById("a2").style.display = "none"
+    document.getElementById("a1").style.display = "none"
+    document.getElementById("a4").style.display = "none"
+}
+function D() {
+    document.getElementById("a4").style.display = "block"
+    document.getElementById("a2").style.display = "none"
+    document.getElementById("a3").style.display = "none"
+    document.getElementById("a1").style.display = "none"
+}
